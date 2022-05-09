4eae3b2f072d09ef2225d156dd0df21011ad8068
Liam <98435043+Filajabob@users.noreply.github.com>
2022-05-08 21:59:07 -0400

    Now shows closing value if market is closed

--- watch/main.js
+++ watch/main.js
@@ -19,17 +19,24 @@ var percentageChange;
 var prevNumChange;
 var prevPerChange;
 
+var comparingTo;
+
 async function updateVals() {
     var valueDisplay = document.getElementById("value-display");
-    var numericalChangeEl = document.getElementById("numerical-change")
-    var percentageChangeEl = document.getElementById("percentage-change")
+    var numericalChangeEl = document.getElementById("numerical-change");
+    var percentageChangeEl = document.getElementById("percentage-change");
+
+    var closeNumChangeEl = document.getElementById("close-numerical-change");
+    var closePerChangeEl = document.getElementById("close-percentage-change");
 
     // Update trading value
     $.getJSON(url, async function(result) {
         if (!result["open"]) {
-            document.getElementById("open-status").textContent = "Market closed."
+            document.getElementById("open-status").textContent = "After hours. Comparing against yesterday's close."
+            comparingTo = result["previousClose"]
         } else {
-            document.getElementById("open-status").textContent = "Market open."
+            document.getElementById("open-status").textContent = "Market open. Comparing against yesterday's close."
+            comparingTo = result["closingVal"]
         }
 
         valueDisplay.classList.remove("valUp");
@@ -40,10 +47,37 @@ async function updateVals() {
 
         await timer(200);
 
-        numericalChange = -(parseFloat(result["closingVal"]) - parseFloat(result["tradingValue"])).toFixed(2);
-        percentageChange = (Math.abs(result["closingVal"] - result["tradingValue"]) / result["closingVal"] * 100).toFixed(2);
+        numericalChange = -(parseFloat(comparingTo - parseFloat(result["tradingValue"]))).toFixed(2);
+        percentageChange = (Math.abs(comparingTo - result["tradingValue"]) / comparingTo * 100).toFixed(2);
+
+        closeNumericalChange = -(parseFloat(result["previousClose"] - parseFloat(result["closingVal"]))).toFixed(2);
+        closePercentageChange = (Math.abs(result["previousClose"] - result["closingVal"]) / result["previousClose"] * 100).toFixed(2);
 
         valueDisplay.innerText = result["tradingValue"].toFixed(2);
+
+        // Handle close data
+        if (!result["open"]) {
+            document.getElementById("at-close").textContent = result["closingVal"]
+            document.getElementById("at-close-context").textContent = "At close. Comparing against yesterday's close."
+
+            // Display numerical change
+            if (closeNumericalChange < 0) {
+                closeNumChangeEl.innerText = closeNumericalChange
+                closeNumlChangeEl.style.color = "red"
+            } else {
+                closeNumChangeEl.innerText = '+' + closeNumericalChange
+                closeNumChangeEl.style.color = "rgb(22, 219, 4)"
+            }
+
+            // Display percentage change
+            if (closeNumericalChange < 0) {
+                closePerChangeEl.innerText = "(-" + closePercentageChange + '%)';
+                closePerChangeEl.style.color = "red";
+            } else {
+                closePerChangeEl.innerText = '(+' + closePercentageChange + '%)';
+                closePerChangeEl.style.color = "rgb(22, 219, 4)";
+            }
+        }
 
         if (result["tradingValue"] < previousValue) {
             // Value goes down
@@ -77,12 +111,12 @@ async function updateVals() {
         previousValue = result["tradingValue"];
         tradingVal = previousValue;
 
-        document.title = ticker + " - " + result["tradingValue"]
+        document.title = `${ticker} - ${result["tradingValue"]} | ${numericalChangeEl.innerText} ${percentageChangeEl.innerText}`
     });
 }
 
 $(document).ready(async function() {
-    $.getJSON("https://FakeStockGenerator.filajabob123.repl.co/ticker/" + ticker, function(res) {
+    $.getJSON(`https://FakeStockGenerator.filajabob123.repl.co/ticker/${ticker}`, function(res) {
         document.getElementById("page-header").innerText = res["name"]
     })
 
